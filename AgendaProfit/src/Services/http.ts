adc41fe2aecb7ab95bd2a1bf5a6a1e63ea0a414e
Wartelon   Malthus <wartelon.malthus@gmail.com>
2025-06-02 21:47:49 -0300

    ajustando front para apontar pra api em deploy

--- AgendaProfit/src/Services/http.ts
+++ AgendaProfit/src/Services/http.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const http = axios.create({
-  baseURL: 'https://localhost:7253/', 
+  //baseURL: 'https://localhost:7253/'
+  baseURL:'https://agendaprofit-backend.onrender.com/', 
   headers: {
     'Content-Type': 'application/json'
   }
