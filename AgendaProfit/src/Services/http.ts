673a10cc982d7d36b62cad84631f06abc79aa283
Wartelon   Malthus <wartelon.malthus@gmail.com>
2025-06-02 22:03:48 -0300

    apontando para localhost

--- AgendaProfit/src/Services/http.ts
+++ AgendaProfit/src/Services/http.ts
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const http = axios.create({
-  //baseURL: 'https://localhost:7253/'
-  baseURL:'https://agendaprofit-backend.onrender.com/', 
+  baseURL: 'https://localhost:7253/',
+  //baseURL:'https://agendaprofit-backend.onrender.com/', 
   headers: {
     'Content-Type': 'application/json'
   }
